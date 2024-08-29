06790d7242e836d8c40cf42e3f20138ee5152a00
GracieGe <ruiyi.ge2001@gmail.com>
2024-08-29 21:09:41 +0800

    direct to statistics-information page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: '/statistics-information',
+      permanent: false
+    }
+  }
+}
+
 const Home = () => {
   return <>Home Page</>
 }
