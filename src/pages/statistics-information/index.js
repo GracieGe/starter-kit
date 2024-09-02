7f23a8a276a7b3824d729ddd2e5e6796c704921a
GracieGe <ruiyi.ge2001@gmail.com>
2024-09-03 00:35:42 +0800

    change the page

--- src/pages/statistics-information/index.js
+++ src/pages/statistics-information/index.js
@@ -1,27 +1,12 @@
 // ** MUI Imports
 import Grid from '@mui/material/Grid'
 
-// ** Icon Imports
-import Icon from 'src/@core/components/icon'
-
 // ** Custom Components Imports
 import CardStatisticsCharacter from 'src/@core/components/card-statistics/card-stats-with-image'
-import CardStatisticsVerticalComponent from 'src/@core/components/card-statistics/card-stats-vertical'
 
 // ** Styled Component Import
 import ApexChartWrapper from 'src/@core/styles/libs/react-apexcharts'
 
-// ** Demo Components Imports
-import CrmTotalSales from 'src/views/pages/statistics-information/CrmTotalSales'
-import CrmWeeklySales from 'src/views/pages/statistics-information/CrmWeeklySales'
-import CrmTotalGrowth from 'src/views/pages/statistics-information/CrmTotalGrowth'
-import CrmUpgradePlan from 'src/views/pages/statistics-information/CrmUpgradePlan'
-import CrmTransactions from 'src/views/pages/statistics-information/CrmTransactions'
-import CrmRevenueReport from 'src/views/pages/statistics-information/CrmRevenueReport'
-import CrmSalesOverview from 'src/views/pages/statistics-information/CrmSalesOverview'
-import CrmMeetingSchedule from 'src/views/pages/statistics-information/CrmMeetingSchedule'
-import CrmDeveloperMeetup from 'src/views/pages/statistics-information/CrmDeveloperMeetup'
-import CrmActivityTimeline from 'src/views/pages/statistics-information/CrmActivityTimeline'
 
 const data = [
   {
@@ -52,54 +37,6 @@ const CRMDashboard = () => {
         </Grid>
         <Grid item xs={12} sm={6} md={3} sx={{ pt: theme => `${theme.spacing(12.25)} !important` }}>
           <CardStatisticsCharacter data={data[1]} />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <CrmTransactions />
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <CrmTotalSales />
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <CrmRevenueReport />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <CrmSalesOverview />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <CrmActivityTimeline />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Grid container spacing={6}>
-            <Grid item xs={12} sm={8}>
-              <CrmWeeklySales />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Grid container spacing={6}>
-                <Grid item xs={6} sm={12}>
-                  <CrmTotalGrowth />
-                </Grid>
-                <Grid item xs={6} sm={12}>
-                  <CardStatisticsVerticalComponent
-                    stats='862'
-                    trend='negative'
-                    trendNumber='-18%'
-                    title='New Project'
-                    subtitle='Yearly Project'
-                    icon={<Icon icon='mdi:briefcase-variant-outline' />}
-                  />
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item xs={12} md={6} lg={4}>
-          <CrmUpgradePlan />
-        </Grid>
-        <Grid item xs={12} md={6} lg={4}>
-          <CrmMeetingSchedule />
-        </Grid>
-        <Grid item xs={12} md={6} lg={4}>
-          <CrmDeveloperMeetup />
         </Grid>
       </Grid>
     </ApexChartWrapper>
